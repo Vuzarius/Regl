6221eafa8d2972dba14aa40ab8fa5e9f20badb53
Vuzarius <vusariusoriginal@gmail.com>
2026-02-26 01:32:28 +0200

    update codes.js

--- codes.js
+++ codes.js
@@ -1,2 +1,2 @@
 // codes.js — автогенерируется ботом. Не редактировать вручную.
-const PORTAL_CODES = {};
+const PORTAL_CODES = {"927519847884148797|admin": {"code": "3491", "key": "AdminKey-Vexus-2025-Secure", "exp": 1772062647}};
